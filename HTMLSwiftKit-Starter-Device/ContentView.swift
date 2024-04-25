02971b76a7eef0673febc27cafc6c663ec788c07
Gokul Nair <gokul.getpowerplay.in>
2024-04-25 12:24:25 +0530

    Initial Commit

---
+++ HTMLSwiftKit-Starter-Device/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HTMLSwiftKit-Starter-Device
+//
+//  Created by Gokul Nair on 25/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HTMLSwiftKit-Starter-Device/HTMLSwiftKit_Starter_DeviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTMLSwiftKit_Starter_DeviceApp.swift
+//  HTMLSwiftKit-Starter-Device
+//
+//  Created by Gokul Nair on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HTMLSwiftKit_Starter_DeviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
